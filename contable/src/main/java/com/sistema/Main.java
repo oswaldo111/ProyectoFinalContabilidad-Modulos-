e248d90fd4c8e52ba5b4d955f6be9c11de903df9
DM24001-2026 <143828903+bran503@users.noreply.github.com>
2026-05-03 17:10:53 +0200

    Test
    Test

--- contable/src/main/java/com/sistema/Main.java
+++ contable/src/main/java/com/sistema/Main.java
@@ -2,6 +2,6 @@ package com.sistema;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido al sistema contable");
     }
 }
