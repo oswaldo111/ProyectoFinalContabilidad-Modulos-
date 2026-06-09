ca262779c48ed4e38dabd2d12acce86ad397a1cb
Cabrera Zaldana Mynor Xavier <cz24003@ues.edu.sv>
2026-06-09 16:01:56 -0600

    PanelRegistroPartida y PartidaController

---
+++ contable/src/main/java/com/sistema/modulos/contabilidad/Controllers/PartidaController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sistema.modulos.contabilidad.Controllers;
+
+public class PartidaController {
+    
+    // TODO: conectar con ContabilidadService de Charlie
+    // private ContabilidadService servicio;
+    
+    public PartidaController() {
+        // inicializar servicio cuando Charlie lo tenga listo
+    }
+    
+    public void guardarPartida() {
+        // TODO: llamar a servicio.guardarPartida()
+        System.out.println("Guardando partida...");
+    }
+    
+    public void validarPartida() {
+        // TODO: llamar a servicio para validar partida doble
+        System.out.println("Validando partida...");
+    }
+}
+
